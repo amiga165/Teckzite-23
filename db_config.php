33f919943da442c78dd832cdba4e3712e19f0159
JUST-VAMSI <veeramallavamsi72@gmail.com>
2023-03-15 09:15:38 +0530

    small error rectified

--- db_config.php
+++ db_config.php
@@ -1,6 +1,8 @@
 <?php
+
 $db_host = 'localhost';
-$db_user = 'root_teckzite';
-$db_password = 'Welcome@!mb888';
-$db_db = 'teckzite';
+$db_user = 'root';
+$db_password = '';
+$db_db = 'teckzite';
+
 ?>
